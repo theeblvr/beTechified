625a752901e215e75dfbd7c06ed42ca1c9215f9c
Ogbe Matthew <ogbematthew1@gmail.com>
2026-09-10 20:42:16 +0100

    Intro Assignment1

---
+++ assignment1/intro.js
@@ -0,0 +1,7 @@
+const name = "Ayomide Matthew Ogbe";
+const age = 20;
+const fav = "Python"
+
+console.log(`My name is ${name}`);
+console.log(`I'm ${age} years old`)
+console.log(`Favourite programming language is ${fav}`)
